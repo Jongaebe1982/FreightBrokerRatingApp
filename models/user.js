155269e114e2c09cf6b32f9e5344d380c7ca68ba
Jon Gaebe <Jon.Gaebe@jbhunt.com>
2020-03-14 14:37:05 -0500

    redeploying to heroku

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, Sequelize) => {
       salt: Sequelize.TEXT,
       roleId: {
         type: Sequelize.INTEGER,
-        defaultValue: 2
+        defaultValue: 1
       }
     },
     {
